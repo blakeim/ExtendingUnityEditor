568063ced2e8cd7af7f4d3bfb9b26bd559a6c833
Ian Blake <IanMBlake13@gmail.com>
2018-11-05 22:02:25 -0500

    Innitial commit

---
+++ Assets/Editor/HelloWorldEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(HelloWorld))]
+public class HelloWorldEditor : Editor {
+
+	public override void OnInspectorGUI(){
+
+		var script = target as HelloWorld;
+
+		script.speed = EditorGUILayout.Slider("Speed", script.speed, 0f, 10f);
+		
+		script.target = EditorGUILayout.ObjectField("Target", script.target, typeof(HelloWorld), true) as HelloWorld;
+		
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("A Button");
+		if (GUILayout.Button("Click me")){
+			Debug.Log(script.message);
+		}
+
+		EditorGUILayout.EndHorizontal();
+		
+		//DrawDefaultInspector();
+
+		//EditorGUILayout.LabelField("Custom Message", script.message);
+	}
+}
---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomDataStructure{
+	public string name;
+	public GameObject target;
+	public Vector3 position;
+}
+
+
+public class HelloWorld : MonoBehaviour {
+
+	[Range(0f, 10f)]
+	public float speed; 
+	public Vector3 startPos;
+
+	[Tooltip("What the game object should follow")]
+	public HelloWorld target;
+	public string message = "Hello world";
+	public CustomDataStructure[] customTarget;
+
+	[Header("Array tests")]
+	public string[] strings;
+	public int[] ints;
+	public Vector3[] vectors;
+	public GameObject[] gameObjects;
+	[HideInInspector]
+	public List<string> stringList;
+	public Dictionary<string, Vector3> childrenPosition;
+
+	public int life{get; set;}
+}
